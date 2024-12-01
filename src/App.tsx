2f2e0558b10b428758b8b8f672f3333278b99add
yosr <yassourayaakoubi@gmail.com>
2024-12-01 19:22:39 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+interface Vehicle {
+  make: string;
+  model: string;
+  year: number;
+  start(): void;
+}
+
+class Car implements Vehicle {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start(): void {
+    console.log("Car engine started");
+  }
+}
+
+const App: React.FC = () => {
+  const myCar = new Car("Mercedes", "G63", 2024);
+
+  myCar.start(); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Car App</h1>
     </div>
   );
 }
